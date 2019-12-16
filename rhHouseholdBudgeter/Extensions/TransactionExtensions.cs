782f6e32222c92d76c90c1c31aef419912776ece
robhercarloz <rhernandezroberto@Gmail.com>
2019-12-16 12:46:44 -0500

    financialPortalUpdate

--- rhHouseholdBudgeter/Extensions/TransactionExtensions.cs
+++ rhHouseholdBudgeter/Extensions/TransactionExtensions.cs
@@ -20,45 +20,46 @@ namespace rhHouseholdBudgeter.Extensions
 
         public static void ManageNotifications(this Transaction transaction)
         {
-            var currentBal = transaction.BankAccount.CurrentBalance;
 
-            if (currentBal < 0)
+            var bankAccount = db.BankAccounts.AsNoTracking().FirstOrDefault(b => b.Id == transaction.BankAccountId);
+            var currentBal = bankAccount.CurrentBalance;
+
+            if (currentBal < 0) 
+            {
                 //SendOverDraftNotification(transaction);
-                var body = $"Your most recent transaction in the amount of {transaction.Amount} has overdrafted account {transaction.BankAccount.Name} leaving you with a currrent balance of {transaction.BankAccount.CurrentBalance}" 
+                var body = $"Your most recent transaction in the amount of {transaction.Amount} has overdrafted account {transaction.BankAccount.Name} leaving you with a currrent balance of {transaction.BankAccount.CurrentBalance}";
                 CreateNotification(transaction, "You over drafted your account", body);
+            }
 
-
-            else if (currentBal > 0 && currentBal <= transaction.BankAccount.lowLevelBalance)
+            else if (currentBal > 0 && currentBal <= transaction.BankAccount.lowLevelBalance) 
+            {
                 //SendOverDraftNotification(transaction);
 
-                var body = $"Your most recent transaction in the amount of {transaction.Amount} has overdrafted account {transaction.BankAccount.Name} leaving you with a currrent balance of {transaction.BankAccount.CurrentBalance}"
-                CreateNotification(transaction, "You over drafted your account", body);
+                var body = $"Your most recent transaction in the amount of {transaction.Amount} has triggered a low balance warning account {transaction.BankAccount.Name} leaving you with a currrent balance of {transaction.BankAccount.CurrentBalance}";
+                CreateNotification(transaction, "You have hit your low balance", body);
+            }
         }
 
         private static void CreateNotification(Transaction transaction, string body, string subject)
         {
+
+
+            var houseId = db.Users.Find(transaction.OwnerId).HouseholdId ?? 0;
             var notification = new Notification
             {
                 Body = body,
                 Created = DateTime.Now,
-                HouseholdId = transaction.BankAccount.HouseholdId,
+                HouseholdId = houseId,
                 IsRead = false,
+                RecipientId = transaction.OwnerId,
                 Subject = subject
             };
             db.Notifications.Add(notification);
         }
 
-
-
-        public static void SendOverDraftNotification(Transaction transaction)
-        {
-            
-        }
-
-
-
         private static void UpdateBankBalance(Transaction transaction)
         {
+
             var bank = db.BankAccounts.Find(transaction.BankAccountId);
             if (transaction.TransactionType == TransactionType.Deposit)
                 bank.CurrentBalance += transaction.Amount;
@@ -80,7 +81,6 @@ namespace rhHouseholdBudgeter.Extensions
             budget.CurrentAmount += transaction.Amount;
             db.SaveChanges();            
         }
-               
 
         private static void UpdateBudgetItemBalance(Transaction transaction)
         {
